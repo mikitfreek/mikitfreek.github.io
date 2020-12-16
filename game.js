68e23d053f5e78d51c0c89a2d95e454651c9e991
Mikołaj Osial <UL0249739@edu.uni.lodz.pl>
2020-12-16 17:38:54 +0100

    Initial commit

--- game.js
+++ game.js
@@ -12,66 +12,29 @@ document.body.appendChild( renderer.domElement );
 
 // Camera
 // method 1 - use lookAt (better performace)
-camera.position.set( 20, 20, 20 );
-camera.lookAt( scene.position );
+//camera.position.set( 20, 20, 20 );
+//camera.lookAt( scene.position );
 // method 2 - set the x-component of rotation
-/*camera.position.set( 20, 20, 20 );
+camera.position.set( 20, 20, 20 );
 camera.rotation.order = 'YXZ';
 camera.rotation.y = - Math.PI / 4;
-camera.rotation.x = Math.atan( - 1 / Math.sqrt( 2 ) );*/
+camera.rotation.x = Math.atan( - 1 / Math.sqrt( 2 ) );
 
 // Controls
-var posX = 0, posZ = 2, posY = 0;
+let posX = 0, posZ = 3, posY = 0;
 
-//
 controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.addEventListener( 'change', animate );
 controls.enableRotate = false;
 controls.enableZoom = true;
-controls.enablePan = false;
+controls.enablePan = true;
 controls.maxPolarAngle = Math.PI / 2;
-
-document.onkeydown = detectKey;
-function detectKey(e) {
-    //var posLeft = document.getElementById('myId').offsetLeft;
-    //var posTop = document.getElementById('myId').offsetTop;
-    e = e || window.event;
-    if (e.keyCode == '38') {
-        // up arrow
-        posX -= 0.3;
-        posY -= 0.3;
-    }
-    else if (e.keyCode == '40') {
-        // down arrow
-        posX += 0.3;
-        posY += 0.3;
-        console.log("down")
-    }
-    else if (e.keyCode == '37') {
-        // left arrow
-        posX -= 0.3;
-        posY += 0.3;
-    }
-    else if (e.keyCode == '39') {
-        // right arrow
-        posX += 0.3;
-        posY -= 0.3;
-    }
-    cube2.position.set(posX, posZ, posY);
-}
 
 // Objects
 
-/*/cube1
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-cube.position.set(posX, posY, posZ);
-scene.add( cube );*/
-
 //cube2
-var geometry2 = new THREE.BoxGeometry( 2, 6, 2 );
-var material2 = new THREE.MeshNormalMaterial();
+const geometry2 = new THREE.BoxGeometry( 2, 6, 2 );
+const material2 = new THREE.MeshNormalMaterial();
 cube2 = new THREE.Mesh( geometry2, material2 );
 cube2.position.set(posX, posZ, posY);
 scene.add( cube2 );
@@ -80,24 +43,24 @@ scene.add( cube2 );
 scene.add( new THREE.AmbientLight( 0x444444 ) );
 
 // Light
-var light = new THREE.PointLight( 0xffffff, 0.8 );
+const light = new THREE.PointLight( 0xffffff, 0.8 );
 light.position.set( 0, 50, 50 );
 scene.add( light );
 
 // Axes
-scene.add( new THREE.AxesHelper( 40 ) );
+//scene.add( new THREE.AxesHelper( 40 ) );
 
 // Grid
-var geometryGrid = new THREE.PlaneBufferGeometry( 100, 100, 10, 10 );
-var materialGrid = new THREE.MeshBasicMaterial( { wireframe: true, opacity: 0.1, transparent: true } );
-var grid = new THREE.Mesh( geometryGrid, materialGrid );
+const geometryGrid = new THREE.PlaneBufferGeometry( 100, 100, 10, 10 );
+const materialGrid = new THREE.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: true, opacity: 0.15, transparent: true } );
+const grid = new THREE.Mesh( geometryGrid, materialGrid );
 grid.rotation.order = 'YXZ';
 grid.rotation.y = - Math.PI / 2;
 grid.rotation.x = - Math.PI / 2;
 scene.add( grid );
 
 // animate
-function animate() {
+function animate () {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 }
