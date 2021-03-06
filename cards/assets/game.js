5c5c0455919eff1748908c33a812173d83d6cf3b
Mikołaj Osial <UL0249739@edu.uni.lodz.pl>
2021-03-06 16:51:25 +0100

    Initial commit

---
+++ cards/assets/game.js
@@ -0,0 +1,16 @@
+var virtualScene;
+var VirtualScene = Class.extend({
+    // Class constructor
+    init: function () {
+        'use strict';
+
+    },
+    // Event handlers
+    setControls: function () {
+        'use strict';
+
+    }
+});
+
+virtualScene = new VirtualScene();
+
